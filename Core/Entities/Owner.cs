86aa4c743f055d37f8861ea4621e159cd64776ae
Pixel <Pixel@PC>
2022-10-07 09:34:25 +0000

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Core.Entities
+{
+  public  class Owner :EntityBase
+    {
+        public String FullName { get; set; }
+        public String Profil { get; set; }
+        public String Avatar { get; set; }
+        public Address Address { get; set; }
+    }
+}
+
+
---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core.Entities
+{
+  public  class PortfolioItem : EntityBase
+    {
+        public String ProjectName { get; set; }
+        public String Description { get; set; }
+        public String ImageUrl { get; set; }
+    }
+}
+
+
